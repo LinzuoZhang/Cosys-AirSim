927a357c2b93b1f7ebf12536afead83d7c79ab47
LZZhang <zhanglinzuo@sjtu.edu.cn>
2025-11-15 20:21:42 +0800

    feat: update UEMessage type to FString for better compatibility with Unreal Engine

--- PythonClient/multirotor/ue_msg.py
+++ PythonClient/multirotor/ue_msg.py
@@ -5,7 +5,7 @@ import time
 
 client = airsim.MultirotorClient()
 client.confirmConnection()
-client.enableApiControl(True)
+# client.enableApiControl(True)
 client.client.call("sendUEMessageToVehicle", "P","drone_1")
 time.sleep(3)
 client.client.call("sendUEMessageToVehicle", "G","drone_1")
